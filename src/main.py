39ef5f3f3edc674392eaf4bde51490765d880dcc
Alina Zabavkina <alina.zabavkina@quantori.com>
2024-07-17 18:28:17 +0300

    Feature implemented

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from rdkit import Chem
 
-SMILES_container = ["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"]
-SMILES_structure = "c1ccccc1"
+smiles_container = ["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"]
+smiles_structure = "c1ccccc1"
 
-def substructure_search(SMILES_container, SMILES_structure):
-    molecule_list = [Chem.MolFromSmiles(x) for x in SMILES_container]
-    substructure = Chem.MolFromSmiles(SMILES_structure)
+def substructure_search(smiles_container, smiles_structure):
+    molecule_list = [Chem.MolFromSmiles(x) for x in smiles_container]
+    substructure = Chem.MolFromSmiles(smiles_structure)
     matches = [Chem.MolToSmiles(x) for x in molecule_list if x.HasSubstructMatch(substructure)]
     return matches
 
-print(substructure_search(SMILES_container, SMILES_structure))
+print(substructure_search(smiles_container, smiles_structure))
