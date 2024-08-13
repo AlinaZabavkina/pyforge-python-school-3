76fffc254d6e4b2b052a478e4881bc64a7e7d6e0
Alina Zabavkina <alina.zabavkina@quantori.com>
2024-08-13 21:52:42 +0300

    Fixed error

--- src/main.py
+++ src/main.py
@@ -83,7 +83,7 @@ async def upload_file(file: UploadFile):
             # Create an instance of the Molecule class
             structure = Chem.MolFromSmiles(mol)
             if structure is None:
-                raise HTTPException(status_code=400, detail=f"Invalid SMILES substructure in your file: {row}")
+                raise HTTPException(status_code=400, detail=f"Invalid SMILES substructure in your file: {mol}")
             else:
                 molecule_instance = MoleculeCreate(molecule_id=int(molecule_id), smiles_structure=mol)
                 # Convert the instance to a dictionary
